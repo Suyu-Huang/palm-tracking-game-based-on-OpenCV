5e3d2fedb7d74789beed20caaeadede7c5996266
黄苏渝 <527524079@qq.com>
2017-01-03 23:20:18 +0800

    Initial Commit

---
+++ opencv_test001/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_test001
+//
+//  Created by 黄苏渝 on 17/1/3.
+//  Copyright © 2017年 黄苏渝. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
